8428f764e9ec719022c692ca718eb105a295f7da
Jeferson Tavares <jefersontavaressilva@gmail.com>
2023-12-21 15:48:14 -0300

    Loop ou Iteração

--- force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
+++ force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
@@ -3,6 +3,8 @@ import { LightningElement, track } from "lwc";
 export default class ConditionalRenderingExample extends LightningElement {
   @track displayDiv = false;
 
+  @track cityList = ["Sâo Paulo", "São Caetano", "Itu", "Rio", "Taubate"];
+
   showDivHandler(event) {
     this.displayDiv = event.target.checked;
   }
